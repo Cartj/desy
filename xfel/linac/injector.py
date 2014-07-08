f159670467809ab951842cceb6767dfcb736ffb4
Ilya Agapov <ilya.agapov@xfel.eu>
2014-07-08 16:26:10 +0200

    linac

--- xfel/linac/injector.py
+++ xfel/linac/injector.py
@@ -1,7 +1,5 @@
 __author__ = 'Sergey Tomin'
 import sys
-ind = sys.path[0].find("desy")
-sys.path.append(sys.path[0][:ind])
 
 from ocelot.cpbd.elements import *
 from ocelot.cpbd.optics import *
@@ -10,15 +8,29 @@ exec(open("injector.inp"))
 
 lat = MagneticLattice(lattice)
 
+'''
 for elem in lat.sequence:
     if elem.type == "cavity":
         print elem.type, elem.f, elem.v, elem.delta_e
-
+'''
 
 tws0 = Twiss()
 tws0.beta_x = 1
 tws0.beta_y = 1
 tws0.alpha_x = 0
 tws0.alpha_y = 0
+
+tws0.E = 0.001 * GeV
+
+# first assign energies to elements
 tws = twiss(lat, tws0)
 plot_opt_func(lat, tws)
+
+lat.update_transfer_maps()
+
+tws = twiss(lat, tws0)
+plot_opt_func(lat, tws)
+
+plt.show()
+
+
