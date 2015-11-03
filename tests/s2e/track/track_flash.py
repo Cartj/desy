a1a5a479af32e8cc711495eb920ead6e39704a6d
Igor Zagorodnov <igor.zagorodnov@desy.de>
2015-11-03 17:39:33 +0100

    local test

--- tests/s2e/track/track_flash.py
+++ tests/s2e/track/track_flash.py
@@ -133,7 +133,7 @@ beam.tlen=2e-3 # in m
 
 
 tw0 = Twiss(beam)
-exec(open('lattice_FLASH_S2E.txt'))
+exec(open('/lattice_FLASH_S2E.txt'))
 lat = MagneticLattice(lattice)
 tws=twiss(lat, tw0, nPoints=None)
 
