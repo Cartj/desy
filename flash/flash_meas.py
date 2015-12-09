74645f8c3a61f704cca97058bf90807cf2c564f3
sergey-tomin <tomin.sergey@gmail.com>
2015-12-09 16:28:13 +0100

    minor

--- flash/flash_meas.py
+++ flash/flash_meas.py
@@ -29,8 +29,8 @@ beam.emit_y = beam.emit_yn / (beam.E / m_e_GeV)
 
 tw0 = Twiss(beam)
 
-#BPM1TCOL.type="drift"
-#BPM2UND3.type="drift"
+BPM1TCOL.type="drift"
+BPM2UND3.type="drift"
 #BPM14SMATCH.type="drift"
 
 lat = MagneticLattice(lattice, start=STARTACC39)
@@ -114,11 +114,11 @@ for elem in lat.sequence:
         Y.append(elem.dy)
         w.append(elem.l)
 S = arange(len(X))
-w = 0.5
+w = 0.3
 plt.figure(1)
-plt.bar(S, X, w, color="r" )
+plt.bar(S+w, X, w, color="r" )
 #plt.figure(2)
-plt.bar(S-0.5, Y, w, color="b" )
+plt.bar(S, Y, w, color="b" )
 plt.show()
 for elem in lat.sequence:
     if elem.type == "quadrupole":
