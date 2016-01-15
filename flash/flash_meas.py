e047518a99c6bb5f43791ebe603f66c2fe9e4501
sergey <tomin.sergey@gmail.com>
2016-01-15 16:20:14 +0100

    minor

--- flash/flash_meas.py
+++ flash/flash_meas.py
@@ -46,11 +46,11 @@ lat = MagneticLattice(lattice, start=STARTACC39)
 setup = log.MachineSetup()
 
 ampls1, phases1 = mi.get_cavity_info(["M1.ACC1"])
-ampls2, phases2 = mi.get_cavity_info(["M2.ACC1"])
+#ampls2, phases2 = mi.get_cavity_info(["M2.ACC1"])
 
 
-beam.E = ampls1[0]*cos(phases1[0]*pi/180.)*0.001 + ampls2[0]*cos(phases2[0]*pi/180.)*0.001 + gun_energy
-print "ACC1 = ", ampls1[0]*cos(phases1[0]*pi/180.)*0.001 + ampls2[0]*cos(phases2[0]*pi/180.)*0.001
+beam.E = ampls1[0]*cos(phases1[0]*pi/180.)*0.001  + gun_energy
+print "ACC1 = ", ampls1[0]*cos(phases1[0]*pi/180.)*0.001
 read_cavs(lat, mi)
 E = beam.E
 for elem in lat.sequence:
