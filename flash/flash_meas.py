01fb5be336e4b91b34569d987a303eb13cbc739d
sergey-tomin <tomin.sergey@gmail.com>
2016-01-17 15:21:24 +0100

    minor

--- flash/flash_meas.py
+++ flash/flash_meas.py
@@ -108,7 +108,7 @@ for elem in lat.sequence:
 
 
 lat.update_transfer_maps()
-
+"""
 constr = {Q6TCOL:{'beta_x':38.25, 'beta_y':18.44}, Q8SMATCH:{'beta_x':14.657, 'beta_y':4.07}}
 vars = [[tw0, 'beta_x'], [tw0, 'beta_y'], [tw0, 'alpha_x'], [tw0, 'alpha_y']]
 match(lat, constr, vars, tw0, xtol=1e-5)
@@ -120,7 +120,7 @@ for elem in lat.sequence:
     if elem in constr.keys():
         constr_pos.append(L)
 print constr_pos
-
+"""
 tws=twiss(lat, tw0)
 #for tw in tws:
 #    if tw.s in constr_pos:
