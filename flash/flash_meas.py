57305c43a203033a90b7432a19d92b96ffc84f5b
sergey-tomin <tomin.sergey@gmail.com>
2016-01-18 19:22:42 +0100

    minor

--- flash/flash_meas.py
+++ flash/flash_meas.py
@@ -98,7 +98,7 @@ for elem in lat.sequence:
         if elem.mi_id in ["Q4DBC2","Q9ACC2", 'Q3.5ECOL', 'Q5UND1.3.5', "Q5UND2.4", 'Q6UND1']:
             k1 = abs(k1)*sign(elem.k1)
         #K1 = k1
-        print(elem.id,  "ideal: k1 = ", elem.k1, " real k1 = ", k1, "I = ", elem.I)
+        print(elem.id,  "ideal: k1 = ", elem.k1, " real k1 = ", k1, "I = ", elem.I, "E = ", E)
         #print(elem.id,  "ideal: k1 = ", elem.k1, " real k1 = ", K1, " dk/k = ", (K1-elem.k1)/elem.k1*100.)
         elem.k1 = k1
     #elif elem.type in ["hcor", "vcor"]:
