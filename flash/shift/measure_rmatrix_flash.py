be0fc533990c43809a8a144a239d3434229f5750
sergey-tomin <tomin.sergey@gmail.com>
2016-01-31 18:19:57 +0100

    minor

--- flash/shift/measure_rmatrix_flash.py
+++ flash/shift/measure_rmatrix_flash.py
@@ -59,9 +59,57 @@ setup.load_lattice(filename, lat)
 tws=twiss(lat, tw0)
 plot_opt_func(lat, tws, top_plot=["Dx"])
 
+
+horizantal = [#'H3DBC3',
+              #'H10ACC4', 'H10ACC5', 'H10ACC6', 'H10ACC7', 'H18ACC7',
+              #'H1TCOL', 'H4TCOL',
+              #'H8TCOL',
+              #'H2ECOL', 'H4ECOL', 'H6ECOL',                     # in dogleg
+              'H5ORS', 'H10ORS', 'H3SFELC', 'H4SFELC',
+              'H10SMATCH', 'H12SMATCH'
+              ]
+
+vertical = [#'V3DBC3',
+            #'V10ACC4' 'V10ACC5', 'V10ACC6', 'V10ACC7',
+            #'V1TCOL', 'V2TCOL', 'V4TCOL',
+            #'V8TCOL',
+            #'V4ECOL',                                                  #in dogleg
+            'V5ORS', 'V6.4ORS', 'V7ORS', 'V9ORS', 'V11ORS', 'V12ORS',
+            'V2SFELC', 'V4SFELC', 'V6SFELC',
+            'V7SMATCH', 'V14SMATCH'
+            ]
+
+bpms = [#'1DBC3', '3DBC3', '9ACC4', '9ACC5', '9ACC6', '11ACC7', '15ACC7', '19ACC7',
+        # '1TCOL', '6TCOL', '8TCOL',
+         #'3ECOL', '5ECOL',                              # dogleg
+        '2ORS', '7ORS', '9ORS', '12ORS',
+        '1SFUND2', '1SFUND3', '1SFUND4', '1SFELC', '1SMATCH', '6SMATCH',
+        '13SMATCH', '14SMATCH', '5UND1', '5UND2', '5UND3', '5UND4', '5UND5', '5UND6'  #undulator section
+        ]
+
 orb = Orbit(lat)
 
+#rmatrix = orb.measure_response_matrix(p_init=Particle(E=beam.E))
+#rmatrix.save("test_r.txt")
+
+#rmatrix.extract(cor_list=horizantal, bpm_list=bpms)
+
+orb.create_COR(cor_list=np.append(horizantal, vertical))
+orb.create_BPM(bpm_list=bpms)
+#orb.export_response_matrix(rmatrix)
 rmatrix = orb.response_matrix(TestInterface(), dp)
 rmatrix.save(rmat_filename)
 
 rmatrix.show()
+
+
+
+#H10SMATCH.angle = 0.001
+
+#orb.read_virtual_orbit(Particle(E=beam.E))
+#plt.plot([b.s for b in orb.bpms], [b.x for b in orb.bpms], "ro-")
+#orb.correction(Particle(E=beam.E))
+#orb.read_virtual_orbit(Particle(E=beam.E))
+#plt.plot([b.s for b in orb.bpms], [b.x for b in orb.bpms], "bo-")
+#plt.show()
+
